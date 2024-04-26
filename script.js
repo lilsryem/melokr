dfc83ba8f838924174e7ce3a06d1fd7b887e9a7d
lilsryem <168193484+lilsryem@users.noreply.github.com>
2024-04-26 22:27:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Liste de paragraphes au hasard
+var paragraphs = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+];
+
 // Attend que la page soit chargée
 window.addEventListener("load", function () {
     // Cache le loader une fois que la page est chargée
@@ -11,6 +20,13 @@ window.addEventListener("load", function () {
     getUserIP(function (ip) {
         userIP.textContent = ip;
     });
+
+    // Affiche un paragraphe au hasard
+    var randomParagraph = document.getElementById("randomParagraph");
+    randomParagraph.style.display = "block";
+    var randomText = document.getElementById("randomText");
+    var randomIndex = Math.floor(Math.random() * paragraphs.length);
+    randomText.textContent = paragraphs[randomIndex];
 });
 
 // Fonction pour récupérer l'adresse IP de l'utilisateur
